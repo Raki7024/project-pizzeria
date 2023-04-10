b8b8fae39491e1704621eb86dffc08738bbc9d31
Piotr Rakus <rakus-piotr@wp.pl>
2023-04-10 10:32:31 +0200

    Add orderProcess in js

--- src/js/script.js
+++ src/js/script.js
@@ -146,7 +146,6 @@
     
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log('formData', formData);
     
       // set price to default price
       let price = thisProduct.data.price;
@@ -155,13 +154,20 @@
       for(let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(paramId, param);
     
         // for every option in this category
         for(let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
+
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          if(optionSelected) {
+            if (!option.default) {
+              price += option.price;
+            }
+          } else if (option.default) {
+            price -= option.price;
+          }
         }
       }
     
